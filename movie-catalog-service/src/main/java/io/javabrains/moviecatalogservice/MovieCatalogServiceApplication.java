6f31b3c45f6172530f4df1f204b3054ec61e4caf
Marco Salernitano <masal.weblogins@gmail.com>
2024-09-08 17:30:31 +0200

    Added Hystrix annotations
    Also added then commented code in Application to configure timeout for RestTemplate

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,13 +2,17 @@ package io.javabrains.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,6 +22,10 @@ public class MovieCatalogServiceApplication {
 	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate() {
+//		// set timeout for requests to other services
+//		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory  = new HttpComponentsClientHttpRequestFactory();
+//		clientHttpRequestFactory.setConnectTimeout(3000);
+//		return new RestTemplate(clientHttpRequestFactory);
 		return new RestTemplate();
 	}
 }
