ef79a2a3faceb057f8de17c2fd078aa1e80fedd9
Marco Salernitano <masal.weblogins@gmail.com>
2024-09-08 19:55:02 +0200

    Split getCatalog service fallback
    Modify getCatalog of the movie-catalog-service in 2 separate calls to 2 separate services class.
    The calls to services have to be in stand alone classes to allow to Hystrix to wrap a proxy around them. Putting methods with fallbacks in the same class of getCatalog wouldn't allow Hystrix to intercept service malfunctions or timeouts.

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/services/MovieInfo.java
@@ -0,0 +1,29 @@
+package io.javabrains.moviecatalogservice.services;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class MovieInfo {
+
+    private RestTemplate restTemplate;
+
+    public MovieInfo(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @HystrixCommand(fallbackMethod = "getFallBackCatalogItem")
+    public CatalogItem getCatalogItem(Rating rating) {
+        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+        return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+    }
+
+    private CatalogItem getFallBackCatalogItem(Rating rating) {
+        return new CatalogItem("Movie name not found for Id: " + rating.getMovieId(), "", rating.getRating());
+    }
+
+}
