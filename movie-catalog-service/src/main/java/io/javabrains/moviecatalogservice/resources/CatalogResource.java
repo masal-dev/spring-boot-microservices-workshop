9e61431a3cb6932a1e6fe35454456949ffd30dff
Marco Salernitano <masal.weblogins@gmail.com>
2024-09-08 19:55:02 +0200

    Split getCatalog service fallback
    Modify getCatalog of the movie-catalog-service in 2 separate calls to 2 separate services class.
    The calls to services have to be in stand alone classes to allow to Hystrix to wrap a proxy around them. Putting methods with fallbacks in the same class of getCatalog wouldn't allow Hystrix to intercept service malfunctions or timeouts.

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -1,19 +1,15 @@
 package io.javabrains.moviecatalogservice.resources;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.javabrains.moviecatalogservice.models.CatalogItem;
-import io.javabrains.moviecatalogservice.models.Movie;
-import io.javabrains.moviecatalogservice.models.Rating;
 import io.javabrains.moviecatalogservice.models.UserRating;
+import io.javabrains.moviecatalogservice.services.MovieInfo;
+import io.javabrains.moviecatalogservice.services.UserRatingInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,30 +18,27 @@ import java.util.stream.Collectors;
 public class CatalogResource {
 
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
     WebClient.Builder webClientBuilder;
 
+    private MovieInfo movieInfo;
+    private UserRatingInfo userRatingInfo;
+
+    public CatalogResource(MovieInfo movieInfo, UserRatingInfo userRatingInfo) {
+        this.movieInfo = movieInfo;
+        this.userRatingInfo = userRatingInfo;
+    }
 
     @RequestMapping("/{userId}")
-    @HystrixCommand(fallbackMethod = "getFallBackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
-        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
-
+        UserRating userRating = userRatingInfo.getUserRating(userId);
         return userRating.getRatings().stream()
                 .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+                    return movieInfo.getCatalogItem(rating);
                 })
                 .collect(Collectors.toList());
-
     }
 
-    public List<CatalogItem> getFallBackCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("No movie", "", 0));
-    }
+
 }
 
 /*
